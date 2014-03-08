bc483e5176ff073cafdf5324e0739f88f705b29f
Miles Victors <milesvictors@gmail.com>
2014-03-08 11:23:33 -0700

    Stubs out directories for bulk of site

---
+++ app/scripts/controllers/budget.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('budgetPeaceApp')
+  .controller('BudgetCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/income.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('budgetPeaceApp')
+  .controller('IncomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
